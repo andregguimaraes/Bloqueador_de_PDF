46c6e660a28ee3a492405ccea87a7b8d1e3bf147
André Gomes <aandre.ggomes55@gmail.com>
2021-09-01 17:43:33 -0300

    Bloqueador de PDFs

---
+++ Main.py
@@ -0,0 +1,13 @@
+import sys
+from EncryptPDF import EncryptPDF
+
+if __name__ == '__main__':
+    if sys.argv.count == 0:
+        exit
+    print("Iniciando processo...")
+    encrypt = EncryptPDF()
+    senha = sys.argv[sys.argv.index('-S') + 1]
+    caminho = sys.argv[sys.argv.index('-C') + 1]
+
+    encrypt.Criptografa(caminho, senha)
+    print("Processo concluído, verifique o arquivo de Log para detalhes...")
